ad774d0216f5a8a07cc43db56f75ae43380acb6d
Trevor L. Davis <trevor.l.davis@gmail.com>
2025-02-28 23:37:56 -0800

    fix: Avoid opening a new graphics device
    * To prevent `marquee::marquee_grob()` from opening a new graphics device
      we open a `pdf(NULL)` device first and then close it `on.exit()`.

--- R/zzz.r
+++ R/zzz.r
@@ -1,6 +1,7 @@
 #' @import grid
 #' @importFrom bittermelon as_bm_pixmap bm_replace bm_trim
 #' @importFrom dplyr filter mutate
+#' @importFrom grDevices dev.off pdf
 #' @importFrom pnpmisc pdf_create_jacket
 #' @importFrom rlang abort check_dots_empty .data
 #' @importFrom stringr str_c str_glue str_sub str_sub<-
