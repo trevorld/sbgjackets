41922648f2aaec70f5e984160517cfe9724f0ffa
Trevor L. Davis <trevor.l.davis@gmail.com>
2026-07-03 22:57:27 -0700

    tests: Skip `pcw_blorg_in_the_midwest()` test if cache does not have rules file

--- tests/testthat/test-blorg_in_the_midwest.R
+++ tests/testthat/test-blorg_in_the_midwest.R
@@ -1,5 +1,6 @@
 test_that("`pcw_blorg_in_the_midwest()`", {
 	skip_if_not(has_runtime_dependencies())
+	skip_if_not(cache_has("Blorg_in_the_Midwest_rules.pdf"))
 	f <- pcw_blorg_in_the_midwest()
 	expect_true(file.exists(f))
 	unlink(f)
